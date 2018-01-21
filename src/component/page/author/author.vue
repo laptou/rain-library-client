a8f6d2571e4952301e82ccfa1a17aae42313f526
Ibiyemi Abiodun <ibiyemi@intulon.com>
2018-01-20 19:33:16 -0500

    properly formatted the author page, finally

--- src/component/page/author/author.vue
+++ src/component/page/author/author.vue
@@ -10,32 +10,35 @@
                         <span class="subtitle">Author</span>
                     </div>
                 </header>
-                <div id="content-wrapper" v-bar>
-                    <section id="content">
-                        <table id="info">
-                            <tr v-if="author.bio">
-                                <td>Bio</td>
-                                <td class="text-secondary">
-                                    <p>
-                                        {{ author.bio }}
-                                    </p>
-                                </td>
-                            </tr>
-                            <tr v-if="author.wiki">
-                                <td>Wiki</td>
-                                <td class="text-secondary"><a :href="author.wiki">Wikipedia</a></td>
-                            </tr>
-                        </table>
+                <div id="content-scroll-wrapper" v-bar>
+                    <div id="content-wrapper">
+                        <section id="content">
+                            <table class="info">
+                                <tr v-if="author.bio">
+                                    <td>Bio</td>
+                                    <td class="text-secondary">
+                                        <p>
+                                            {{ author.bio }}
+                                        </p>
+                                    </td>
+                                </tr>
+                                <tr v-if="author.wiki">
+                                    <td>Wiki</td>
+                                    <td class="text-secondary"><a :href="author.wiki">Wikipedia</a></td>
+                                </tr>
+                            </table>
 
-                        <h2>Books</h2>
+                            <h2>Books</h2>
 
-                        <ul v-if="books">
-                            <li v-for="book in books" :key="book.id">
-                                {{ book.name }}
-                            </li>
-                        </ul>
-                        
-                    </section>
+                            <ul v-if="books" class="tile-list tile-small">
+                                <li v-for="book in sortedBooks" :key="book.id">
+                                    {{ book.name }}<br/>
+                                    <span class="subtitle">ree</span>
+                                </li>
+                            </ul>
+                            
+                        </section>
+                    </div>
                 </div>
                 <section id="actions">
                     <button class="btn-auxilary" @click="$router.go(-1)">
@@ -58,6 +61,18 @@ export default class AuthorPage extends Vue
 {
     author: Person | null = null;
     books: Book[] | null = [];
+
+    get sortedBooks(): Book[] | null
+    {
+        if (this.books)
+        {
+            const books = this.books;
+            books.sort((a, b) => a.name.localeCompare(b.name));
+            return books;
+        }
+
+        return null;
+    }
 
     @vue.Lifecycle
     created()
