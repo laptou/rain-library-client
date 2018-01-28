a0ea08d19a9267fd7f2f6f921ee06f669c09ac83
Ibiyemi Abiodun <ibiyemi@intulon.com>
2018-01-27 20:34:58 -0500

    made a batch file for publishing

---
+++ publish.bat
@@ -0,0 +1,11 @@
+echo off
+cls
+webpack --config webpack.vendor-bundles.js
+webpack --config webpack.prod.js
+echo
+echo Build completed, beginning copy
+echo
+robocopy ./ "\\192.168.3.173\Node Server\client" webpack.*.js config.js
+robocopy ./dist "\\192.168.3.173\Node Server\client\dist" /mir
+echo
+echo Copy completed
