d560e3c30d338c1a7f92d1ba4b9f3ea6842959dd
Ibiyemi Abiodun <ibiyemi@intulon.com>
2018-01-30 20:12:38 -0500

    reorganized homepage menu + tweaked page design

--- src/component/control/acrylic/acrylic.vue
+++ src/component/control/acrylic/acrylic.vue
@@ -10,27 +10,30 @@
 import * as vue from "av-ts";
 import Vue from "vue";
 
-enum Mode
-{
+enum Mode {
     Image = "image",
     Color = "color"
 }
 
 @vue.Component
-export default class Acrylic extends Vue
-{
-    @vue.Prop background = vue.p({ required: true });
-    @vue.Prop mode: string | Mode = vue.p({ type: String, default: Mode.Image });
+export default class Acrylic extends Vue {
+    @vue.Prop background = vue.p({ type: String });
+    @vue.Prop
+    mode: string | Mode = vue.p({ type: String, default: Mode.Image });
 
-    get style()
-    {
+    get style() {
         return this.mode === Mode.Color
             ? { "background-color": this.background }
-            : { "background-image": this.background };
+            : {
+                  "background-image":
+                      this.background ||
+                      this.$store.getters["ui/background/url-blurred"]
+              };
     }
 }
 </script>
 
 <style lang="scss" scoped src="./acrylic.scss">
+
 
 </style>
--- src/component/page/home/home.vue
+++ src/component/page/home/home.vue
@@ -1,151 +1,153 @@
 <template>
     <div id="root" v-bar>
-        <div>
-            <ul id="menu-container">
-                <li v-if="user">
-                    <button class="btn-auxilary">
-                        Welcome, {{ user.name.first + " " + user.name.last }}
-                    </button>
-                </li>
+    <div>
+        <div id="menu-container" :class="{ 'menu-open': menuOpen }">
+        <acrylic>
+            <button id="menu-button" @click="menuOpen = !menuOpen" aria-hidden>
+                &#x2630;
+            </button>
+        </acrylic>
+        
+        <ul>
+            <li v-if="user">
+                <button class="btn-auxilary">
+                    Welcome, {{ user.name.first + " " + user.name.last }}
+                </button>
+            </li>
 
-                <router-link tag="li" to="/admin" v-if="user && user.permissions.indexOf('admin') !== -1">
+            <router-link tag="li" to="/admin" v-if="user && user.permissions.indexOf('admin') !== -1">
+                <a>
                     <button class="btn-primary">
                         Manage Library
                     </button>
-                </router-link>
-                
-                <li  v-if="user">
-                    <a href="javascript:void(0)" @click="$store.dispatch('auth/logout')">
-                        <button class="btn-secondary">
-                            Log Out
-                        </button>
-                    </a>
-                </li>
+                </a>
+            </router-link>
 
-                <router-link v-else to="/login" tag="li">
-                        <button class="btn-primary">
-                            Log In
-                        </button>
-                </router-link>
-            </ul>
+            <li v-if="user">
+                <a href="javascript:void(0)" @click="$store.dispatch('auth/logout')">
+                    <button class="btn-secondary">
+                        Log Out
+                    </button>
+                </a>
+            </li>
 
-            <div id="logo-container">
-                <div id="logo-box">
-                    <acrylic :background="backgroundAcrylic" :mode="'image'">
-                        <div id="logo-wrapper">
-                            <div id="logo-border" :class="`logo-${theme}`">
-                                <div id="title-container">
-                                    <h1 id="title">
-                                        RAIN <br/>
-                                        INSTITUTE <br/>
-                                        LIBRARY
-                                    </h1>
-                                </div>
+            <router-link v-else to="/login" tag="li">
+                <a>
+                    <button class="btn-primary">
+                        Log In
+                    </button>
+                </a>
+            </router-link>
+        </ul>
+        </div>
+
+        <div id="logo-container">
+            <div id="logo-box">
+                <acrylic>
+                    <div id="logo-wrapper">
+                        <div id="logo-border" :class="`logo-${theme}`">
+                            <div id="title-container">
+                                <h1 id="title">
+                                    RAIN
+                                    <br/> INSTITUTE
+                                    <br/> LIBRARY
+                                </h1>
                             </div>
                         </div>
-                    </acrylic>
-                </div>
+                    </div>
+                </acrylic>
             </div>
+        </div>
 
-            <div id="content-container">
-                
-                <div id="search-container">
-                    <autocomplete :itemsSource="suggestions"
-                                  :itemLabelSelector="label"
-                                  :itemDescriptionSelector="describe"
-                                  :itemTagSelector="() => null"
-                                  :itemTemplateSelector="template"
-                                  :placeholder="'search for books, authors, and more!'"
-                                  :acrylic-background="background"
-                                  @querychanged="onQueryChanged">
+        <div id="content-container">
 
-                    </autocomplete>
-                </div>
-                <div id="info-container">
-                    <section id="info-checkout" v-if="user">
-                        <h2>Checked out</h2>
-                        <ul v-if="checkedOut.length">
-                            <router-link tag="li"
-                                         v-for="checkout in checkedOut"
-                                         :key="checkout._id"
-                                         :to="`/book/${checkout.book.isbn}`"
-                                         class="checkout"
-                                         :class="{ overdue: Date.parse(checkout.due) <= new Date() }">
-                                <div class="content">
-                                    <span class="book-name">{{ checkout.book.name }}</span>
-                                    <span class="book-authors flat-list no-wrap">
-                                        <span v-for="(author, index) in checkout.book.authors" :key="author._id">
-                                            {{ author.name | name }}{{ index + 1 < checkout.book.authors.length ? "," : null }}
-                                        </span>
+            <div id="search-container">
+                <autocomplete :itemsSource="suggestions" :itemLabelSelector="label" :itemDescriptionSelector="describe" :itemTagSelector="() => null"
+                    :itemTemplateSelector="template" :placeholder="'search for books, authors, and more!'" :acrylic-background="background"
+                    @querychanged="onQueryChanged">
+
+                </autocomplete>
+            </div>
+            <div id="info-container">
+                <section id="info-checkout" v-if="user">
+                    <h2>Checked out</h2>
+                    <ul v-if="checkedOut.length">
+                        <router-link tag="li" v-for="checkout in checkedOut" :key="checkout._id" :to="`/book/${checkout.book.isbn}`" class="checkout"
+                            :class="{ overdue: Date.parse(checkout.due) <= new Date() }">
+                            <div class="content">
+                                <span class="book-name">{{ checkout.book.name }}</span>
+                                <span class="book-authors flat-list no-wrap">
+                                    <span v-for="(author, index) in checkout.book.authors" :key="author._id">
+                                        {{ author.name | name }}{{ index + 1
+                                        < checkout.book.authors.length ? "," : null
+                                            }} </span>
                                     </span>
                                     <span class="book-genre text-secondary">
                                         <span v-for="(genre, index) in checkout.book.genre" :key="genre">
-                                            {{ genre }}{{ index + 1 < checkout.book.genre.length ? "," : null }}
+                                            {{ genre }}{{ index + 1
+                                            < checkout.book.genre.length ? "," : null }} </span>
                                         </span>
-                                    </span>
-                                </div>
-                                <span class="tag tag-info"
-                                      v-if="Date.parse(checkout.due) > new Date()">
-                                    <span>{{ checkout.due | relative-time }}</span>
-                                    <span class="subtitle">remaining</span>
-                                </span>
-                                <span class="tag tag-danger" v-else>
-                                    overdue
-                                </span>
-                            </router-link>
-                        </ul>
-                        <span class="empty-message" v-else>
-                            Go to the library to check out some books!
-                        </span>
-                    </section>
-                    
-                    <section id="info-trending">
-                        <h2>Trending</h2>
-                        <span class="empty-message">
-                            This feature hasn't been implemented yet.
-                        </span>
-                    </section>
+                            </div>
+                            <span class="tag tag-info" v-if="Date.parse(checkout.due) > new Date()">
+                                <span>{{ checkout.due | relative-time }}</span>
+                                <span class="subtitle">remaining</span>
+                            </span>
+                            <span class="tag tag-danger" v-else>
+                                overdue
+                            </span>
+                        </router-link>
+                    </ul>
+                    <span class="empty-message" v-else>
+                        Go to the library to check out some books!
+                    </span>
+                </section>
 
-                    <section id="info-holds" v-if="user">
-                        <h2>Holds</h2>
-                        <ul v-if="holds.length">
-                            <router-link tag="li"
-                                         v-for="hold in holds"
-                                         :key="hold._id"
-                                         :to="`/book/${hold.book.isbn}`">
-                                <div class="content">
-                                    <span class="book-name">{{ hold.book.name }}</span>
-                                    <span class="book-authors flat-list no-wrap">
-                                        <span v-for="(author, index) in hold.book.authors" :key="author._id">
-                                            {{ author.name | name }}{{ index + 1 < hold.book.authors.length ? "," : null }}
-                                        </span>
+                <section id="info-trending">
+                    <h2>Trending</h2>
+                    <span class="empty-message">
+                        This feature hasn't been implemented yet.
+                    </span>
+                </section>
+
+                <section id="info-holds" v-if="user">
+                    <h2>Holds</h2>
+                    <ul v-if="holds.length">
+                        <router-link tag="li" v-for="hold in holds" :key="hold._id" :to="`/book/${hold.book.isbn}`">
+                            <div class="content">
+                                <span class="book-name">{{ hold.book.name }}</span>
+                                <span class="book-authors flat-list no-wrap">
+                                    <span v-for="(author, index) in hold.book.authors" :key="author._id">
+                                        {{ author.name | name }}{{ index + 1
+                                        < hold.book.authors.length ? "," : null }} </span>
                                     </span>
                                     <span class="book-genre text-secondary">
                                         <span v-for="(genre, index) in hold.book.genre" :key="genre">
-                                            {{ genre }}{{ index + 1 < hold.book.genre.length ? "," : null }}
+                                            {{ genre }}{{ index + 1
+                                            < hold.book.genre.length ? "," : null }} </span>
                                         </span>
-                                    </span>
-                                </div>
-                                <span class="tag tag-primary" v-if="hold.ready">
-                                    ready for pickup
-                                </span>
-                            </router-link>
-                        </ul>
-                        <span class="empty-message" v-else>
-                            You don't have any books on hold.
-                        </span>
-                    </section>
-                </div>
+                            </div>
+                            <span class="tag tag-primary" v-if="hold.ready">
+                                ready for pickup
+                            </span>
+                        </router-link>
+                    </ul>
+                    <span class="empty-message" v-else>
+                        You don't have any books on hold.
+                    </span>
+                </section>
             </div>
-
-            <footer id="attribution-container" >
-                <span v-if="backgroundInfo && backgroundInfo.author">
-                    Photo by <a :href="backgroundInfo.author.url">{{ backgroundInfo.author.name }}</a><br/>
-                </span>
-                © 2017-2018 Ibiyemi Abiodun
-            </footer>
         </div>
+
+        <footer id="attribution-container">
+            <span v-if="backgroundInfo && backgroundInfo.author">
+                Photo by
+                <a :href="backgroundInfo.author.url">{{ backgroundInfo.author.name }}</a>
+                <br/>
+            </span>
+            © 2017-2018 Ibiyemi Abiodun
+        </footer>
     </div>
+</div>
 </template>
 
 <script lang="ts">
@@ -164,6 +166,7 @@ export default class HomePage extends Vue {
     suggestions: any[] = [];
     checkedOut: Book[] = [];
     holds: Hold[] = [];
+    menuOpen: boolean = false;
 
     get user(): Person | null {
         return this.$store.state.auth.user;
